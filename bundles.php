e9b165c401b349f12079d564123fcc90efc2be4a
Koen Schmeets <k.schmeets@gmail.com>
2012-05-16 22:39:54 +0200

    Initial import (previously in getlayla/core SHA: 7b720a7d27e94a34a3c5e109d82b94e228a09d0d)

---
+++ bundles.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+	'layla_thirdparty_application' => array(
+		'auto' => true,
+		'location' => path('layla_thirdparty').'application'
+	),	
+
+	'layla_thirdparty_bootsparks' => array(
+		'auto' => true,
+		'location' => path('layla_thirdparty').'bundles'.DS.'bootsparks'
+	),
+
+);
