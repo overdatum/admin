c84dffcf1ea5f47556821d1eed1620899f8b31bb
Koen Schmeets <k.schmeets@gmail.com>
2012-06-27 19:16:33 +0200

    Adding create page for account

--- pages/account.php
+++ pages/account.php
@@ -52,7 +52,36 @@ class Admin_Account_Page {
 
 	public function create($view)
 	{
-		$view->form(Module::form('account.create'), 'POST', prefix('admin').'account/add');
+		$view->form(function($view)
+		{
+			$view->page_header(function($view)
+			{
+				$view->title('ADD ACCOUNT');
+			});
+			
+			// Get Roles and put it in a nice array for the dropdown
+			$roles = array('' => '') + model_array_pluck(API::get(array('roles'))->get('results'), function($role)
+			{
+				return $role->lang->name;
+			}, 'id');
+
+			// Get Languages and put it in a nice array for the dropdown
+			$languages = model_array_pluck(API::get(array('languages'))->get('results'), function($language)
+			{
+				return $language->name;
+			}, 'id');
+
+			$view->text('name',  __('admin::account.create.form.name'), Input::old('name'));
+			$view->text('email', __('admin::account.create.form.email'), Input::old('email'));
+			$view->password('password', __('admin::account.create.form.password'));
+			$view->multiple('roles[]', __('admin::account.create.form.roles'), $roles, Input::old('roles'));
+			$view->dropdown('language_id', __('admin::account.create.form.language'), $languages, Input::old('language_id'));
+
+			$view->actions(function($view)
+			{
+				$view->submit(__('admin::account.create.buttons.add'), 'primary');
+			});
+		}, 'POST', prefix('admin').'account/add');
 	}
 
 	public function update($view, $account)
